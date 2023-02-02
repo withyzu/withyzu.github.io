394e8739a9b5ca9f6fb467187616f57fcc7bcab1
withyzu <withyzu@outlook.com>
2023-02-02 23:26:07 +0800

    add

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 window.onload = function () {
   loadCategory();
+  searchBoxVisibility();
 };
-function onresize() {
+window.addEventListener("resize", debounce(reRender, 100)); //监听resize
+function reRender() {
   document.getElementById("category").innerHTML = "";
   loadCategory();
-}
+  searchBoxVisibility();
+  console.log("loging");
+} //重新渲染Category
+function onresize() {}
 function displayTxt() {
   var xmlhttp;
   if (window.XMLHttpRequest) {
@@ -25,7 +30,7 @@ function displayTxt() {
 function displayMd() {
   document.getElementById("showMd").innerHTML = marked.parse("# Marked in the browser\n\nRendered by **marked**.");
 }
-function categoryAddChild() {
+function divAddChild() {
   var category_Width = document.getElementById("category").clientWidth;
   var category_Heigh = document.getElementById("category").clientHeight;
 
@@ -57,7 +62,8 @@ function categoryAddChild() {
   }
 }
 function loadCategory() {
-  var url = "1.json";
+  perRowCellNum_Aspect_Ratio = 0.8; //单元格比例
+  var url = "1.json"; //json文件url
   var j;
   var request = new XMLHttpRequest();
   request.open("GET", url, true);
@@ -69,12 +75,9 @@ function loadCategory() {
     }
 
     var category_Width = document.getElementById("category").clientWidth;
-    var category_Heigh = document.getElementById("category").clientHeight;
-
     var categoryNum = Object.keys(j).length; //此Div中放多少个类目
     console.log(categoryNum);
 
-    perRowCellNum_Aspect_Ratio = 0.8; //单元格比例
     var width_100px_CellNum = parseInt(category_Width / 100); //以100px为基础，计算此Div中可以放多少个宽100px的单元格
     if (width_100px_CellNum < 0) width_100px_CellNum++; //端元个至少为1
     var perRowCellNum = 0; //此Div中每行多少单元格
@@ -84,30 +87,42 @@ function loadCategory() {
     } while (width_100px_CellNum > 0); //当设备尺寸变化，以幂次方增加或减少单元格数量
     console.log(perRowCellNum);
 
-    {
-      c_w = 100 / perRowCellNum + "%"; //单元格宽度
-      c_h = (1 / perRowCellNum) * category_Width * perRowCellNum_Aspect_Ratio + "px"; //单元格高度
-      i = 0;
-      while (i < categoryNum) {
-        var div = document.createElement("div");
-        var div_div = document.createElement("div");
-        var div_div_a = document.createElement("a");
-        var div_div_a_div = document.createElement("div");
-        var div_div_a_div_h1 = document.createElement("h1");
+    c_w = 100 / perRowCellNum + "%"; //单元格宽度
+    c_h = parseFloat(1 / perRowCellNum).toFixed(1) * category_Width * perRowCellNum_Aspect_Ratio + "px"; //单元格高度
+    i = 0;
+    while (i < categoryNum) {
+      var div = document.createElement("div");
+      var div_div = document.createElement("div");
+      var div_div_a = document.createElement("a");
+      var div_div_a_div = document.createElement("div");
+      var div_div_a_div_h1 = document.createElement("h1");
 
-        div_div_a.href = j[i].url;
-        div_div_a_div.style.backgroundImage = "url(" + j[i].imgGrd + ")";
-        div_div_a_div_h1.textContent = j[i].name;
+      div_div_a.href = j[i].url;
+      div_div_a_div.style.backgroundImage = "url(" + j[i].imgGrd + ")";
+      div_div_a_div_h1.textContent = j[i].name;
 
-        div_div_a_div.appendChild(div_div_a_div_h1);
-        div_div_a.appendChild(div_div_a_div);
-        div_div.appendChild(div_div_a);
-        div.appendChild(div_div);
-        div.style.width = c_w;
-        div.style.height = c_h;
-        document.getElementById("category").appendChild(div);
-        i++;
-      }
+      div_div_a_div.appendChild(div_div_a_div_h1);
+      div_div_a.appendChild(div_div_a_div);
+      div_div.appendChild(div_div_a);
+      div.appendChild(div_div);
+      div.style.width = c_w;
+      div.style.height = c_h;
+      document.getElementById("category").appendChild(div);
+      i++;
     }
   };
-}
+} //加载category
+function debounce(fn, wait) {
+  timer = null;
+  return function () {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(fn, wait);
+  };
+} //防抖
+function searchBoxVisibility() {
+  b_w = window.innerWidth;
+  document.getElementById("searchBox").style.display = b_w > 300 ? "block" : "none";
+  console.log("...");
+} //当设备宽度小于200，searchBox不显示
