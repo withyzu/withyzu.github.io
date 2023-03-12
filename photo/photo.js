694de1d82c6999ead9c5003dbf018053456dcf76
withyzu <withyzu@outlook.com>
2023-03-12 16:11:14 +0800

    updata

--- photo/photo.js
+++ photo/photo.js
@@ -1,3 +1,5 @@
+window.addEventListener("resize", debounce(layout_fit_device, 50), false); //令布局适配设备;
+
 function keyword_display_onclick() {
   var e1 = document.getElementById("keyword-pad");
   var e2 = document.getElementById("keyword-display");
@@ -9,6 +11,7 @@ function keyword_display_onclick() {
     e1.style.height = "var(--fix-icon-w-h)";
     e1.style.bottom = "calc(160px - var(--fix-icon-w-h) * 0.5)";
     e1.style.visibility = "hidden";
+    e1.style.opacity = "0";
 
     e2.style.backgroundColor = "var(--accent-color-no-alpha)";
     e2.style.outline = "1.5px var(--foregrd-no-alpha) solid";
@@ -20,10 +23,22 @@ function keyword_display_onclick() {
   //////////////////
   else {
     e1.style.backgroundColor = "var(--keyword-backgrd)";
-    e1.style.width = "calc(100% - var(--fix-icon-w-h))";
-    e1.style.height = "160px";
+    if (document.documentElement.clientWidth < 600) {
+      e1.style.width = "calc(100% - var(--fix-icon-w-h) * 1)";
+    }
+    if (
+      document.documentElement.clientWidth >= 600 &&
+      document.documentElement.clientWidth <= 1400
+    ) {
+      e1.style.width = "calc(100% - var(--fix-icon-w-h) * 1)";
+    }
+    if (document.documentElement.clientWidth > 1400) {
+      e1.style.width = "calc(1200px + var(--fix-icon-w-h) * 4)";
+    }
+    e1.style.height = " calc(var(--fix-icon-w-h) + 240px)";
     e1.style.bottom = "calc(var(--fix-icon-w-h) / 2)";
     e1.style.visibility = "visible";
+    e1.style.opacity = "1";
 
     e2.style.backgroundColor = "transparent";
     e2.style.outline = "0";
@@ -32,4 +47,46 @@ function keyword_display_onclick() {
     e3.style.opacity = "0";
     e4.textContent = "👁️";
   }
+} //关键词面板显示
+
+function debounce(operate, delay) {
+  let data = null;
+  let datar = null;
+  let newTime = null;
+  function task() {
+    newTime = +new Date();
+    if (newTime - data < delay) {
+      datar = setTimeout(task, delay);
+    } else {
+      operate();
+      datar = null;
+    }
+    data = newTime;
+  }
+  return function () {
+    // 更新时间戳
+    data = +new Date();
+    if (!datar) {
+      datar = setTimeout(task, delay);
+    }
+  };
 }
+//防抖
+
+function layout_fit_device() {
+  var e1 = document.getElementById("keyword-pad");
+  if (e1.style.visibility != "hidden") {
+    if (document.documentElement.clientWidth < 600) {
+      e1.style.width = "calc(100% - var(--fix-icon-w-h) * 1)";
+    }
+    if (
+      document.documentElement.clientWidth >= 600 &&
+      document.documentElement.clientWidth <= 1400
+    ) {
+      e1.style.width = "calc(100% - var(--fix-icon-w-h) * 1)";
+    }
+    if (document.documentElement.clientWidth > 1400) {
+      e1.style.width = "calc(1200px + var(--fix-icon-w-h) * 4)";
+    }
+  }
+} //令布局适配设备
