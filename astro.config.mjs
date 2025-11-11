6f1efc99496760079b997db99a1d99cebb95e475
withyzu <withyzu@outlook.com>
2025-11-12 01:14:24 +0800

    yes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 // https://astro.build/config
@@ -12,4 +12,8 @@ export default defineConfig({
   },
 
   integrations: [react()],
+
+
 });
+
+
