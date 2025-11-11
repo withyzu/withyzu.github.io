44ab15f02278f8c5569a04b8f36cad14aeee76b8
withyzu <withyzu@outlook.com>
2025-11-12 00:37:52 +0800

    add  ui.shadcn.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://withyzu.github.io',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
 });
